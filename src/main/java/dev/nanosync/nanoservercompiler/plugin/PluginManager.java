9a7c98179020ff00d150454a40ff6073d2630b3d
✔️Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2022-01-25 14:20:03 -0300

    ⚙️Refactor for move project.

--- src/main/java/dev/nanosync/nanoservercompiler/plugin/PluginManager.java
+++ src/main/java/dev/nanosync/nanoservercompiler/plugin/PluginManager.java
@@ -1,13 +1,10 @@
 package dev.nanosync.nanoservercompiler.plugin;
 
-import dev.nanosync.nanoservercompiler.file.PropertiesManager;
+import dev.nanosync.nanoservercompiler.external.PropertiesManager;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static dev.nanosync.nanoservercompiler.external.ProjectJARManager.move;
 
 public class PluginManager {
     public static void compileProject() throws Exception {
@@ -15,27 +12,6 @@ public class PluginManager {
         Runtime runtime = Runtime.getRuntime();
         runtime.exec(PropertiesManager.getMavenCommand(), new String[]{""}, new File(PropertiesManager.getFolderProject()));
         Thread.sleep(8200);
-        moveProject();
-    }
-
-    public static void moveProject() throws Exception {
-        System.out.println("Copiando o arquivo do target para os plugins...");
-        File targetJarFile = new File(PropertiesManager.getFolderProject() + "/target/" + PropertiesManager.getJarName());
-        if (targetJarFile.exists()){
-            File folderPlugins = new File("plugins");
-            folderPlugins.mkdir();
-            Files.move(Paths.get(targetJarFile.toURI()), Paths.get(System.getProperty("user.dir") + "/plugins/" + PropertiesManager.getJarName()), REPLACE_EXISTING);
-        } else {
-            System.out.println("O arquivo JAR não existe no projeto, verifique as variáveis novamente.");
-        }
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Você deseja realizar o processo novamente? (S/N)");
-        String response = scanner.nextLine();
-        if (response.equalsIgnoreCase("N")) {
-            System.out.println("Encerrando a aplicação...");
-        } else {
-            PluginManager.compileProject();
-        }
+        move("/target/", "/plugins/");
     }
 }
